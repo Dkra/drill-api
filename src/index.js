043a05cfba5f0bd9e594d3739bc827cedaa40895
roger.jiang <dkra4741@gmail.com>
2020-09-27 18:42:16 +0800

    setup dev flow, support nodemon + babel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import article from './article/index.js'
 const app = express()
+
 app.use(bodyParser.json())
+app.use('/article', article)
+
 app.get('/', (req, res) => {
   res.send('Welcome to Node Babel')
 })
+
 app.listen(5000, () => {
   console.log(`app is listening to port 5000`)
 })
