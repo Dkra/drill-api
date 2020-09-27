eeb8e1580b9a271c65a30810d19cc31b09115724
roger.jiang <dkra4741@gmail.com>
2020-09-27 18:54:53 +0800

    use env module

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 import article from './article/index.js'
+
+const { SEVER_RUNNING_PORT } = process.env
+console.log('SEVER_RUNNING_PORT', SEVER_RUNNING_PORT)
 const app = express()
 
 app.use(bodyParser.json())
@@ -10,6 +14,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to Node Babel')
 })
 
-app.listen(5000, () => {
-  console.log(`app is listening to port 5000`)
+app.listen(SEVER_RUNNING_PORT, () => {
+  console.log(`app is listening to port ${SEVER_RUNNING_PORT}`)
 })
