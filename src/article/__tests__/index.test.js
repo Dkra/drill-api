a2bacf22b7004ab610f94688b4b071e9639101f6
roger <roger>
2020-09-29 14:28:49 +0800

    integration test skeleton

--- src/article/__tests__/index.test.js
+++ src/article/__tests__/index.test.js
@@ -1,1 +1,22 @@
-test.todo('article CRUD integration test');
+const { async } = require('regenerator-runtime')
+import axios from 'axios'
+import { buildArticle } from '../../utils/testHelper'
+
+test('Article CRUD integration test', async () => {
+  const body = {
+    title: 'my title',
+    content: 'my content',
+  }
+  const expectedData = buildArticle(body)
+  console.log('expectedData', expectedData)
+  const createdData = await axios.post('http://localhost:5000/article/', body)
+
+  // Create
+  expect(createdData).toMatchObject(expectedData)
+
+  // Read
+
+  // Update
+
+  // Delete
+})
