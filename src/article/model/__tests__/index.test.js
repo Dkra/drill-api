77285319ec01187aa272d798971d62111a50c0f2
Dkra <dkra4741@gmail.com>
2020-11-05 16:31:15 +0800

    fine tune

--- src/article/model/__tests__/index.test.js
+++ src/article/model/__tests__/index.test.js
@@ -1,63 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const databaseName = 'drill'
-
-// describe('Article Model Test', () => {
-//   beforeAll(async () => {
-//     const url = `mongodb://127.0.0.1/${databaseName}`
-//     await mongoose.connect(url, { useNewUrlParser: true })
-//   })
-
-//   it('create & save article successfully', async () => {
-//     const validArticle = new ArticleModel(articleData)
-//     const savedArticle = await validArticle.save()
-
-//     // Object Id should be defined when successfully saved to MongoDB.
-//     expect(savedArticle._id).toBeDefined()
-//     expect(savedArticle.title).toBe(articleData.title)
-//     expect(savedArticle.content).toBe(articleData.content)
-//   })
-
-// it('create & save user successfully', async () => {
-//   const validUser = new UserModel(userData)
-//   const savedUser = await validUser.save()
-//   // Object Id should be defined when successfully saved to MongoDB.
-//   expect(savedUser._id).toBeDefined()
-//   expect(savedUser.name).toBe(userData.name)
-//   expect(savedUser.gender).toBe(userData.gender)
-//   expect(savedUser.dob).toBe(userData.dob)
-//   expect(savedUser.loginUsing).toBe(userData.loginUsing)
-// })
-
-// Test Schema is working!!!
-// You shouldn't be able to add in any field that isn't defined in the schema
-// it('insert user successfully, but the field does not defined in schema should be undefined', async () => {
-//   const userWithInvalidField = new UserModel({
-//     name: 'TekLoon',
-//     gender: 'Male',
-//     nickname: 'Handsome TekLoon',
-//   })
-//   const savedUserWithInvalidField = await userWithInvalidField.save()
-//   expect(savedUserWithInvalidField._id).toBeDefined()
-//   expect(savedUserWithInvalidField.nickkname).toBeUndefined()
-// })
-
-// // Test Validation is working!!!
-// // It should us told us the errors in on gender field.
-// it('create user without required field should failed', async () => {
-//   const userWithoutRequiredField = new UserModel({ name: 'TekLoon' })
-//   let err
-//   try {
-//     const savedUserWithoutRequiredField = await userWithoutRequiredField.save()
-//     error = savedUserWithoutRequiredField
-//   } catch (error) {
-//     err = error
-//   }
-//   expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
-//   expect(err.errors.gender).toBeDefined()
-// })
-// })
-
 import mongoose from 'mongoose'
 import { setupMongooseMongoMemoryDB } from '../../../utils/modelTestingUtils'
 import ArticleModel from '../../db/ArticleModel'
@@ -74,6 +14,7 @@ afterAll(async () => {
   await mongoServer.stop()
 })
 
+// Modle Clean up
 beforeEach((done) => {
   ArticleModel.remove({}, (err) => {
     done()
@@ -93,14 +34,6 @@ describe('Article Model Test', () => {
     expect(data.title).toEqual(articleData.title)
     expect(data.content).toEqual(articleData.content)
   })
-
-  // it('add() should throw error if missing paraterms', async () => {
-  //   console.log('add', add)
-  //   const data = await add({title: '123', /* content: 'content'*/})
-
-  //   expect(data).toBeThrown.add
-  //   expect(data.content).toEqual(articleData.content)
-  // })
 
   it('get()', async () => {
     // Make sure clean-up
